d65c278dc6ef22cc53a4166ea52391c1cbc4dfaf
Gabriel Macedo <gabriel.ol.macedo@hotmail.com>
2022-05-14 08:55:43 -0300

    Testes iniciais com movimentos possiveis das peças.

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -1,7 +1,7 @@
 using System;
 
 namespace tabuleiro{
-    class Peca {
+    abstract class Peca {
         
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
@@ -18,6 +18,8 @@ namespace tabuleiro{
         public void incrementarQtdMovimento(){
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
  
     }
 }
